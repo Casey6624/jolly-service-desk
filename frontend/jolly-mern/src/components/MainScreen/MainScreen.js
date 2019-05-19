bd4cc67e792d95d011604433c77807198c246c2c
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-05-19 23:24:02 +0100

    Using Material Design Admin template for layout. Have implemented the layout and started to rip out unecessary components. Really want the frontend to be as nimble as possible

--- frontend/jolly-mern/src/components/MainScreen/MainScreen.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function MainScreen(props) {
-    return (
-        <div>
-            <h1> Main Screen! </h1>
-        </div>
-    )
-}
---
+++ frontend/src/views/Dashboard/Dashboard.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import PropTypes from "prop-types";
+// @material-ui/core
+import withStyles from "@material-ui/core/styles/withStyles";
+// @material-ui/icons
+import Computer from "@material-ui/icons/Computer";
+import ViewModule from "@material-ui/icons/ViewModule";
+import MarkerCheck from "@material-ui/icons/Done";
+// core components
+import GridItem from "components/Grid/GridItem.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import Tasks from "components/Tasks/Tasks.jsx";
+import CustomTabs from "components/CustomTabs/CustomTabs.jsx";
+
+import dashboardStyle from "assets/jss/material-dashboard-react/views/dashboardStyle.jsx";
+
+var bugs = [
+  'Sign contract for "What are conference organizers afraid of?"',
+  "Lines From Great Russian Literature? Or E-mails From My Boss?",
+  "Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit",
+  "Create 4 Invisible User Experiences you Never Knew About"
+];
+var website = [
+  "Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit",
+  'Sign contract for "What are conference organizers afraid of?"'
+];
+var server = [
+  "Lines From Great Russian Literature? Or E-mails From My Boss?",
+  "Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit",
+  'Sign contract for "What are conference organizers afraid of?"'
+];
+
+class Dashboard extends React.Component {
+  state = {
+    value: 0
+  };
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  handleChangeIndex = index => {
+    this.setState({ value: index });
+  };
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
+            <CustomTabs
+              title="Tasks:"
+              headerColor="primary"
+              tabs={[
+                {
+                  tabName: "ALL",
+                  tabIcon: ViewModule,
+                  tabContent: (
+                    <Tasks
+                      checkedIndexes={[0, 3]}
+                      tasksIndexes={[0, 1, 2, 3]}
+                      tasks={bugs}
+                    />
+                  )
+                },
+                {
+                  tabName: "ACTIVE",
+                  tabIcon: Computer,
+                  tabContent: (
+                    <Tasks
+                      checkedIndexes={[0]}
+                      tasksIndexes={[0, 1]}
+                      tasks={website}
+                    />
+                  )
+                },
+                {
+                  tabName: "COMPLETED",
+                  tabIcon: MarkerCheck,
+                  tabContent: (
+                    <Tasks
+                      checkedIndexes={[1]}
+                      tasksIndexes={[0, 1, 2]}
+                      tasks={server}
+                    />
+                  )
+                }
+              ]}
+            />
+          </GridItem>
+        </GridContainer>
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(dashboardStyle)(Dashboard);
