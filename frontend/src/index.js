50ed4c73cd5b37e46ff69af35263938d23a767bf
Casey Smith <casey@jollyit.co.uk>
2019-07-16 13:32:11 +0100

    Implemented Lazy Loading which works really nicely, however it doesn't seem to load the material UI CSS for the login screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,20 @@
-import React, { useState } from "react"
+import React, { useState, Suspense } from "react"
 import ReactDOM from "react-dom";
 
-import AuthenticatedApp from "./RootAuth/index"
-import UnauthenticatedApp from "./RootUnauth/index"
+// Need to remove this for Lazy Loading to be fully implemented
+import Authenticated from "./RootAuth/index"
+import Unauthenticated from "./RootUnauth/index"
+
+const UnauthenticatedApp = React.lazy(() => import("./RootUnauth/index"))
+const AuthenticatedApp = React.lazy(() => import("./RootAuth/index"))
 
 export default function App() {
   const [auth, setAuth] = useState(null)
-  return auth === null ? <UnauthenticatedApp setAuth={setAuth} /> : <AuthenticatedApp auth={auth} setAuth={setAuth} />
+  return(
+    <Suspense fallback={<div>Loading...</div>} >
+      {!auth ? <UnauthenticatedApp setAuth={setAuth} /> : <AuthenticatedApp auth={auth} setAuth={setAuth} />}
+    </Suspense>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
